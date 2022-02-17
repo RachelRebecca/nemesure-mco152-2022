09a47aeb246c2a3041658dc19a5662bf88b3f7b3
RachelRebecca <RachelRebecca67@gmail.com>
2022-02-16 20:14:46 -0500

    created basis for ScrabbleGame class

---
+++ src/main/java/ScrabbleGame.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class ScrabbleGame
+{
+    private ArrayList<String> playedWords = new ArrayList<>();
+    private ArrayList<Character> tiles = new ArrayList<>();
+    private ScrabbleDictionary dictionary = new ScrabbleDictionary();
+
+    //make a test
+    public ScrabbleGame()
+    {
+        // give the player 7 random files
+    }
+    /**
+     * if word exists in scrabble dictionary,
+     * and the letter exist in the tiles list
+     * remove the letters from the list and add new random letters
+     */
+
+    public boolean playWord(String word)
+    {
+        //keep playing til no more words exist
+
+        return false;
+    }
+}
